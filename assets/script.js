62dc629a078df110b576a940a7c2d0d5ff6a75f3
Your Name <you@example.com>
2022-06-06 09:43:42 -0400

    Declaring Variables

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// Declaring Variables
+
+// There are three ways to declare a variable in JavaScript, and each has a slightly different use case. All three methods follow the same syntax, but use a different keyword
+
+// Variables should be declared that the name represents the data they are storing and manipulating
+
+let myVar = "a Variable";
+
+const myVarAgain = "a Variable";
+
+var myVarThree  = "a Variable";
